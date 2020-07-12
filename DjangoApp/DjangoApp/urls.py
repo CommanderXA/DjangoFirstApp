6e0f0a9a49d9cdcb8be7b54fb9e333525108a747
CommanderXA <53992051+CommanderXA@users.noreply.github.com>
2020-07-12 19:12:03 +0600

    the second tutorial's page complete

--- DjangoApp/DjangoApp/urls.py
+++ DjangoApp/DjangoApp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('home/', include('DjangoFirstApp.urls')),
+    path('polls/', include('DjangoFirstApp.urls')),
     path('admin/', admin.site.urls),
 ]
